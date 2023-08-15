7f0a9327d614ae58b170f71ab468d473ec20b229
Amir Alao <109921747+AmirAlao@users.noreply.github.com>
2023-08-14 22:08:17 -0400

    need help with syntax highlighting
    Could not figure out syntax highlighting

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,22 @@
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 
-import{mdsvex} from "mdsvex"
-
-
+import{escapeSvelte, mdsvex} from "mdsvex"
+import shiki from 'shiki'
 
 /**@type {import ('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: [".md"]
+	extensions: ['.md'],
+	layout: {
+		_: './src/mdsvex.svelte'
+	},
+	// highlight: {
+	// 	highlighter: async (code, lang = 'text') => {
+	// 		const highlighter = await shiki.getHighlighter({ theme: "github-dark" });
+	// 			const html = escapeSvelte(highlighter.codeToHtml(code, {lang}));
+	// 			return `{@html} \`${html}\`}`;
+	// 	},
+	// },
 }
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
