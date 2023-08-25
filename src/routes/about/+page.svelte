889d09fba4996bf1bb25f27c15fe6b24c1d03f2c
Amir Alao <109921747+AmirAlao@users.noreply.github.com>
2023-08-25 18:16:34 -0400

    Updated About
    Updated About

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,10 @@
+<script>
+
+    $: href = `https://www.twitter.com/star_prizzy`
+</script>
+
 <h1> About</h1>
 
-<p> Welcome to my first blog. Bookmark this it will be worth a fortune soon.</p>
+<p> Welcome to my first blog. Bookmark this it will be worth a fortune soon.
+    Heres My <a {href}> Twitter </a>. 
+</p>
