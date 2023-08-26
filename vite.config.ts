60ef970f98bd50cc68207ec1356ae5bea4d90a52
Amir Alao <109921747+AmirAlao@users.noreply.github.com>
2023-08-26 18:31:23 -0400

    FIxed Background bug
    Fixed background bug and Toggle

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
+
+
 
 export default defineConfig({
+	server: {
+		fs: {
+			allow: ['/static/']
+		}
+	},
 	plugins: [sveltekit()]
 })
